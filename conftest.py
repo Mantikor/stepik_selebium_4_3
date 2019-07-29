1414e3d00e2d0d2cfd285272efef091683b4fa3f
Siarhei Straltsou <straltsou.siarhei@gmail.com>
2019-07-29 10:46:51 +0300

    override local path settings

--- conftest.py
+++ conftest.py
@@ -12,9 +12,9 @@ def browser(request):
     user_language = request.config.getoption('language')
     options = Options()
     options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
-    chrome_d_path = '/opt/Chrome/chromedriver'
-    browser = webdriver.Chrome(options=options, executable_path=chrome_d_path)
-    # browser = webdriver.Chrome(options=options)
+    # chrome_d_path = '/opt/Chrome/chromedriver'
+    # browser = webdriver.Chrome(options=options, executable_path=chrome_d_path)
+    browser = webdriver.Chrome(options=options)
     browser.implicitly_wait(5)
     yield browser
     browser.quit()
